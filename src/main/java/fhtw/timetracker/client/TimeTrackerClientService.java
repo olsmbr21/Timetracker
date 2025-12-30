0714f517122b353ff3b2e043374f5b7a30da3734
Kolin Naraci <naracikolin04@gmail.com>
2025-12-30 22:23:26 +0100

    Create the client service class with host and port (no network functions yet)

---
+++ src/main/java/fhtw/timetracker/client/TimeTrackerClientService.java
@@ -0,0 +1,16 @@
+package fhtw.timetracker.client;
+
+/**
+ * Stage 06.1: Client-Service Grundgerüst.
+ *
+ */
+public class TimeTrackerClientService {
+
+    private final String host;
+    private final int port;
+
+    public TimeTrackerClientService(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+}
