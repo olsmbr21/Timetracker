3c3b80a4e3cda330e4d93459f611c107acbe1774
Kolin Naraci <naracikolin04@gmail.com>
2026-01-13 20:16:54 +0100

    AdminTab skeleton, in the next commit comes the UI + Table(Without Logic)

--- src/main/java/fhtw/timetracker/client/AdminTab.java
+++ src/main/java/fhtw/timetracker/client/AdminTab.java
@@ -1,4 +1,18 @@
 package fhtw.timetracker.client;
 
+import fhtw.timetracker.model.Booking;
+import javafx.collections.ObservableList;
+import javafx.scene.layout.VBox;
+
 public class AdminTab {
+
+    private VBox root;
+
+    public AdminTab(TimeTrackerClientService service, ObservableList<Booking> allBookings) {
+        root = new VBox();
+    }
+
+    public VBox getRoot() {
+        return root;
+    }
 }
