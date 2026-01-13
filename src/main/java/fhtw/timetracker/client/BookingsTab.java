f20f34190a635e235efaa023142ee264ee0999d8
Esmeralda Nurce <ic25b002@technikum-wien.at>
2026-01-13 19:52:16 +0100

    BookingsTab skeleton ,in the next commit comes the BookingsTab UI + bookingtable (ohne logik)

--- src/main/java/fhtw/timetracker/client/BookingsTab.java
+++ src/main/java/fhtw/timetracker/client/BookingsTab.java
@@ -1,4 +1,20 @@
 package fhtw.timetracker.client;
 
+import fhtw.timetracker.model.Booking;
+import fhtw.timetracker.model.Task;
+import javafx.collections.ObservableList;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+
 public class BookingsTab {
+
+    private VBox root;
+
+    public BookingsTab(TextField txtUser, ObservableList<Task> tasks, TimeTrackerClientService service, ObservableList<Booking> myBookings) {
+        root = new VBox();
+    }
+
+    public VBox getRoot() {
+        return root;
+    }
 }
