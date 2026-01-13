0bbc776aeadd575f84a1e15faa1753083b83f111
Olsi Mbrozi <ic25b055@technikum-wien.at>
2026-01-13 19:11:51 +0100

    TasksTab skeleton, in the next commit comes the UI Control

--- src/main/java/fhtw/timetracker/client/TasksTab.java
+++ src/main/java/fhtw/timetracker/client/TasksTab.java
@@ -1,4 +1,19 @@
 package fhtw.timetracker.client;
 
+import fhtw.timetracker.model.Task;
+import javafx.collections.ObservableList;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+
 public class TasksTab {
+
+    private VBox root;
+
+    public TasksTab(TextField txtUser, ObservableList<Task> tasks) {
+        root = new VBox();
+    }
+
+    public VBox getRoot() {
+        return root;
+    }
 }
